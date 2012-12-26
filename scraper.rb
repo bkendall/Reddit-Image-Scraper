156d61c9eca16394a839a02787213249f041f40e
Joe Ardent <code@ardent.nebcorp.com>
2012-12-25 17:34:48 -0800

    cleans up status output

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,8 @@ require 'restclient'
 require 'xmlsimple'
 
 iregex = /http:\/\/(i\.){0,1}(minus|imgur).com\/([0-9a-zA-Z]){3,}(\.jpg|\.gif|\.png){0,1}/i
-newfiles = 0
+newfiles = Hash.new 0
+
 
 ARGV.each do |subreddit|
   destination = subreddit
@@ -38,6 +39,8 @@ ARGV.each do |subreddit|
     iregex.match(i["description"][0])
   }
 
+  puts "Checking #{subreddit}"
+
   # download all the files
   links.each { |lnk|
     next if lnk.nil?
@@ -45,23 +48,20 @@ ARGV.each do |subreddit|
     img = (/(jpg|gif|png)$/i).match l
     if !img
       [".jpg", ".gif", ".png"].each { |ending|
-        puts "  Checking #{l}"
         candidate = l.to_s + ending
         i = File.join destination, File.basename(candidate)
         gstr = "#{File.join(destination, File.basename(l.to_s))}*"
-	puts "   gstr: #{gstr}"
         g = Dir.glob gstr
         already_have = g.size > 0
         if already_have
-          puts "Skipping write of #{i}"
           break
         else
           begin
             RestClient.get(candidate) { |response, request, result|
               next unless response.code.to_s.start_with? "200"
               File.open(i, 'w') { |f| f.write response }
-              puts "Writing #{i}"
-              newfiles = newfiles + 1
+              puts "  Wrote #{i}"
+              newfiles[subreddit] = newfiles[subreddit] + 1
             }
           rescue
             next
@@ -72,12 +72,12 @@ ARGV.each do |subreddit|
       i = File.join destination, File.basename(l)
       already_have = File.exists? i
       if already_have
-        puts "Skipping write of #{i}"
+        next
       else
         begin
           File.open(i, 'w') { |f| f.write(RestClient.get(l)) }
-          puts "Writing #{i}"
-          newfiles = newfiles + 1
+          puts "  Wrote #{i}"
+          newfiles[subreddit] = newfiles[subreddit] + 1
         rescue
         end
       end
@@ -85,4 +85,12 @@ ARGV.each do |subreddit|
   }
 end
 
-puts "\nWrote #{newfiles} new files."
+puts ""
+total = 0
+newfiles.keys.sort.each do |k|
+  num = newfiles[k]
+  total = total + num
+  puts "#{k}: #{num}" if num > 0
+end
+
+puts "\nWrote #{total} new files."
